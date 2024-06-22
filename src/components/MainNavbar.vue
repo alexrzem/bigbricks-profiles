fb2bac3492a7fb50ab6df37e2652a5d0b88424b0
Alex <alexrzem@users.noreply.github.com>
2024-06-23 00:30:58 +1000

    disabled

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -32,6 +32,7 @@
               <input
                 id="search"
                 name="search"
+                disabled="true"
                 class="block w-full rounded-md border-0 bg-gray-700 py-1.5 pl-10 pr-3 text-gray-300 placeholder:text-gray-400 focus:bg-white focus:text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
                 placeholder="Search"
                 type="search" />
